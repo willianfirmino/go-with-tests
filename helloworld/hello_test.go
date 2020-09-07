9ab84f8b58917005121b2975898c2681d5bb90fb
William Firmino <willian.firmino1@gmail.com>
2020-09-07 20:43:37 -0300

    Finishing Hello World example.

--- helloworld/hello_test.go
+++ helloworld/hello_test.go
@@ -11,14 +11,25 @@ func TestHello(t *testing.T) {
 	}
 
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("William")
+		got := Hello("William", "")
 		want := "Hello, William"
 		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("saying Hello, World when an empty string is supplied", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Elodie", "French")
+		want := "Bonjour, Elodie"
 		assertCorrectMessage(t, got, want)
 	})
 
